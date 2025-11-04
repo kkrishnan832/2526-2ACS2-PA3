92eb7c74638450a7281a13cb1b69ae2134992e7f
kkrishnan832 <kkrishnan@madeira.org>
2025-11-04 09:48:59 -0500

    11/4, in class
    Put the main function inside a "while not exit" loop, wrote calculations, did some try/except, edited saving history to save each user's info under a new file, edited show history to print the contents of the file.

--- pa3.py
+++ pa3.py
@@ -1,5 +1,10 @@
 '''
-Block comment here
+to do:
+readme
+create alternatives for all the other currencies
+do more try/except
+figure out what's wrong w to_or_from
+loop what happens when file_not_found_error
 '''
 
 convert_to_other_currencies = {
@@ -27,37 +32,55 @@ convert_from_other_currencies = {
 view_options = ["view", "view purchases", "view past purchases", "view my purchases"]
 convert_to_options = ["convert to another currency", "to other currency", "to another currency", "convert to other currency", "convert dollars to another currency", "convert from dollars to another currency", "convert from dollars to other currency"]
 convert_from_options = ["convert from another currency", "from other currency", "from another currency", "convert from other currency", "convert to dollars from another currency", "convert to dollars from other currency"]
-
+exit_options = ["exit", "quit", "to exit", "exiting", "e"]
     
 def calculations(to_or_from):
-    if to_or_from = "convert to":
-        #use convert to dict
-    elif to_or_from = "convert from":    
-        #use convert from dict
+    if to_or_from == "convert to":
+        currency_name = input("What is currency you would like to convert to? ")
+        if currency_name.lower in ['euro', 'euros']:
+            currency_name = "Euro"
+        elif currency_name.lower in ['yen', 'yens', 'japanese yen', 'japanese yens', 'japanese', 'japan']:
+            currency_name = "Japanese yen"
+        #do the same for all the other currencies        
+        initial_value = input("What is the original cost? (In dollars) ")
+        currency_rate = convert_to_other_currencies[currency_name] 
+    elif to_or_from == "convert from":    
+        currency_name = input("What is currency you would like to convert from? ")
+        initial_value = input("What is the original cost? (In another currency) ")
+        currency_rate = convert_from_other_currencies[currency_name]
+    final_currency = initial_value * currency_rate    
+    print(f"The final answer is {final_currency} {currency_name}.")
 
 def saving_history():
     username = input("What is your username?")
-    add_user = open('history.txt', 'a')
-    add_user.write(username)
+    try:
+        open('{username}.txt', 'x') 
+    except FileExistsError:
+        open('{username}.txt', 'a')    
 
 def show_history():
-    #print ts    
-
+    try:
+        user_file = open('{username}.txt', 'r')
+        file_content = user_file.read()
+        print(file_content)
+    except FileNotFoundError:
+        print("Sorry, I couldn't find anything under that username. Please try again.")
+        #loop it    
 
 
 def main():
     print("Hi, welcome to Katie's currency!")
-    options = input("Would you like to view past purchases, convert dollars to another currency, or convert to dollars from another currency?")
-    if options.lower in view_options:
-        show_history()
-    elif options.lower in convert_to_options:
-        to_or_from == "convert to"
-        calculations()
-    elif options.lower in convert_from_options:      
-        to_or_from == "convert from"
-        calculations()
-    else:
-        to_or_from = input("Sorry, I didn't catch that. Would you like to view past purchases, convert dollars to another currency, or convert from dollars to another currency?")
-        #loop it
-    saving_history()     
+    options = input("Would you like to view past purchases, convert dollars to another currency, convert to dollars from another currency, or exit the program? ")
+    while options.lower not in exit_options:
+        while options.lower not in view_options and options.lower not in convert_to_options and options.lower not in convert_from_options:
+            to_or_from = input("Sorry, I didn't catch that. Would you like to view past purchases, convert dollars to another currency, or convert from dollars to another currency?")
+        if options.lower in view_options:
+            show_history()
+        elif options.lower in convert_to_options:
+            to_or_from = "convert to"
+            calculations()
+        elif options.lower in convert_from_options:      
+            to_or_from = "convert from"
+            calculations()
+        saving_history()      
     print("Bye bye!")

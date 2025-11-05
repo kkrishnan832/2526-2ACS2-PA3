9dad195f4a676c4cf889f42f06f7e16c42f8b9ef
kkrishnan832 <kkrishnan@madeira.org>
2025-11-05 10:37:15 -0500

    11/5 - Classtime
    Set up while loops so that try/except will loop until the try portion is fulfilled, fixed positional arguments in main function so that it will be recognized correctly/created dummy variables, tested the create file under username function & it worked!!!

--- pa3.py
+++ pa3.py
@@ -34,72 +34,89 @@ def calculations(to_or_from):
     
     if to_or_from == "convert to":
         currency_name = input("What is currency you would like to convert to? ")     
+        while currency_name.lower() not in currency_options:
+            currency_name = input("Sorry, I didn't recognize that currency. Please try again. ")
         initial_value = input("What is the original cost? (In dollars) ")
-        if currency_name.lower in ['euro', 'euros']:
+        while True:
+            try:
+                int(initial_value)
+                break
+            except ValueError:
+                initial_value = input("I didn't recognize that as a number. Please try again. What is the original cost? (In dollars) ")       
+        if currency_name.lower() in ['euro', 'euros']:
             currency_name = "Euro"
-        elif currency_name.lower in ['yen', 'yens', 'japanese yen', 'japanese yens']:
+        elif currency_name.lower() in ['yen', 'yens', 'japanese yen', 'japanese yens']:
             currency_name = "Japanese yen"
-        elif currency_name.lower in ['pounds', 'british pounds', 'pound', 'british pound', 'pound sterling']:
+        elif currency_name.lower() in ['pounds', 'british pounds', 'pound', 'british pound', 'pound sterling']:
             currency_name = "Pound sterling"
-        elif currency_name.lower in ['cad', 'canadian dollar', 'canadian dollars']:
+        elif currency_name.lower() in ['cad', 'canadian dollar', 'canadian dollars']:
             currency_name = "Canadian dollar"
-        elif currency_name.lower in ['peso', 'pesos', 'mexican pesos', 'mexican peso']:
+        elif currency_name.lower() in ['peso', 'pesos', 'mexican pesos', 'mexican peso']:
             currency_name = "Mexican peso"
-        elif currency_name.lower in ['chinese yuan', 'chinese yen', 'chinese yuans', 'chinese yens', 'yuans']:
+        elif currency_name.lower() in ['chinese yuan', 'chinese yen', 'chinese yuans', 'chinese yens', 'yuans']:
             currency_name = "Chinese yuan"    
-        elif currency_name.lower in ['rands', 'sad', 'rand', 'south african rand', 'south african rands']:
-            currency_name = "South African Rand"
-        elif currency_name.lower in ['rupees', 'rupee', 'ruppee', 'ruppees', 'indian rupee', 'indian rupees']:
-            currency_name = "Indian rupee"  
-        while currency_name.lower not in currency_options:
-            currency_name = input("Sorry, I didn't recognize that currency. Please try again. ")    
+        elif currency_name.lower() in ['rands', 'sad', 'rand', 'south african rand', 'south african rands']:
+            currency_name = "South African rand"
+        elif currency_name.lower() in ['rupees', 'rupee', 'ruppee', 'ruppees', 'indian rupee', 'indian rupees']:
+            currency_name = "Indian rupee"      
         currency_rate = convert_to_other_currencies[currency_name] 
     elif to_or_from == "convert from":    
         currency_name = input("What is currency you would like to convert from? ")
+        while currency_name.lower() not in currency_options:
+            currency_name = input("Sorry, I didn't recognize that currency. Please try again. ")
         initial_value = input("What is the original cost? (In another currency) ")
-        if currency_name.lower in ['euro', 'euros']:
+        while True:
+            try:
+                int(initial_value)
+                break
+            except ValueError:
+                initial_value = input("I didn't recognize that as a number. Please try again. What is the original cost? (In dollars) ")   
+        if currency_name.lower() in ['euro', 'euros']:
             currency_name = "Euro"
-        elif currency_name.lower in ['yen', 'yens', 'japanese yen', 'japanese yens']:
+        elif currency_name.lower() in ['yen', 'yens', 'japanese yen', 'japanese yens']:
             currency_name = "Japanese yen"
-        elif currency_name.lower in ['pounds', 'british pounds', 'pound', 'british pound', 'pound sterling']:
+        elif currency_name.lower() in ['pounds', 'british pounds', 'pound', 'british pound', 'pound sterling']:
             currency_name = "Pound sterling"
-        elif currency_name.lower in ['cad', 'canadian dollar', 'canadian dollars']:
+        elif currency_name.lower() in ['cad', 'canadian dollar', 'canadian dollars']:
             currency_name = "Canadian dollar"
-        elif currency_name.lower in ['peso', 'pesos', 'mexican pesos', 'mexican peso']:
+        elif currency_name.lower() in ['peso', 'pesos', 'mexican pesos', 'mexican peso']:
             currency_name = "Mexican peso"
-        elif currency_name.lower in ['chinese yuan', 'chinese yen', 'chinese yuans', 'chinese yens', 'yuans']:
+        elif currency_name.lower() in ['chinese yuan', 'chinese yen', 'chinese yuans', 'chinese yens', 'yuans']:
             currency_name = "Chinese yuan"    
-        elif currency_name.lower in ['rands', 'sad', 'rand', 'south african rand', 'south african rands']:
-            currency_name = "South African Rand"
-        elif currency_name.lower in ['rupees', 'rupee', 'ruppee', 'ruppees', 'indian rupee', 'indian rupees']:
+        elif currency_name.lower() in ['rands', 'sad', 'rand', 'south african rand', 'south african rands']:
+            currency_name = "South African rand"
+        elif currency_name.lower() in ['rupees', 'rupee', 'ruppee', 'ruppees', 'indian rupee', 'indian rupees']:
             currency_name = "Indian rupee"        
-        while currency_name.lower not in currency_options:
+        while currency_name.lower() not in currency_options:
             currency_name = input("Sorry, I didn't recognize that currency. Please try again. ")      
         currency_rate = convert_from_other_currencies[currency_name]    
-    final_currency = initial_value * currency_rate    
+    final_currency = int(initial_value) * int(currency_rate)    
     print(f"The final answer is {final_currency} in {currency_name}.")
+    return final_currency, currency_name
 
 def saving_history(final_currency, currency_name):
-    username = input("What is your username?")
+    username = input("What is your username? ")
     filename = f"{username}.txt"
     try:
         file = open(filename, 'x') 
     except FileExistsError:
         file = open(filename, 'a')
     x = datetime.datetime.now()  
-    todays_date = (x.strftime("%d/%m/%Y"))  
+    todays_date = (x.strftime("%m/%d/%Y"))  
     file.write(f"{todays_date} - {final_currency}, {currency_name}\n")       
     file.close()     
+    return filename
 
 def show_history(filename):
-    try:
-        user_file = open(filename, 'r')
-    except FileNotFoundError:
-        username = input("Sorry, I couldn't find anything under that username. Please try another. ") 
-        filename = f"{username}.txt"
-        user_file = open(filename, 'r')
+    while True:
+        try:
+            user_file = open(filename, 'r')
+            break
+        except FileNotFoundError:
+            username = input("Sorry, I couldn't find anything under that username. Please try another. ") 
+            filename = f"{username}.txt"
     file_content = user_file.read()
-    print(file_content)    
+    print(file_content)
 
 
 def main():
@@ -110,18 +127,21 @@ def main():
 
     print("Hi, welcome to Katie's currency!")
     options = input("Would you like to view past purchases, convert dollars to another currency, convert to dollars from another currency, or exit the program? ")
-    while options.lower not in exit_options:
-        while options.lower not in view_options and options.lower not in convert_to_options and options.lower not in convert_from_options:
-            to_or_from = input("Sorry, I didn't catch that. Would you like to view past purchases, convert dollars to another currency, or convert from dollars to another currency? ")
-        if options.lower in view_options:
-            show_history()
-        elif options.lower in convert_to_options:
+    while options.lower() not in exit_options:
+        while options.lower() not in view_options and options.lower() not in convert_to_options and options.lower() not in convert_from_options:
+            options = input("Sorry, I didn't catch that. Would you like to view past purchases, convert dollars to another currency, or convert from dollars to another currency? ")
+        if options.lower() in view_options:
+            username = input("What is your username? ")
+            fn = f"{username}.txt"
+            show_history(fn) #repeats
+        elif options.lower() in convert_to_options:
             to_or_from = "convert to"
-            calculations(to_or_from)
-        elif options.lower in convert_from_options:      
+            fc, cn = calculations(to_or_from)
+            saving_history(fc, cn) 
+        elif options.lower() in convert_from_options:      
             to_or_from = "convert from"
-            calculations(to_or_from)
-        saving_history()      
+            fc, cn = calculations(to_or_from)
+            saving_history(fc, cn) 
     print("Bye bye!")
 
 if __name__ == "__main__":

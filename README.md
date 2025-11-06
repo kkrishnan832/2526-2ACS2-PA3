33f1681a4cb11707b4a2c67648db3ab8f59e4e4a
kkrishnan832 <kkrishnan@madeira.org>
2025-11-05 21:17:02 -0500

    11/5, evening
    I finished up my last to-dos, updated my readme, changed some little diction choices in the printed instructions, fixed the main function loop so that the user can perform functions as many times as wanted, added in-line comments, deleted my own username's history file, & added an end message.

--- README.md
+++ README.md
@@ -1,11 +1,43 @@
 # 2526-2ACS2-PA3
-design thinking final passion project
+Design Thinking Final Passion Project
 
 # Empathize: 
 Someone struggles to easily convert between their home country's currencies and the others they encounter. (could be travel, immigration, etc)
 
 # Research
+https://www.ig.com/en/trading-strategies/what-are-the-top-10-most-traded-currencies-in-the-world-200115
+    - Euros, yen, pounds, CAD, pesos, yuan, South African rand, Indian rupees
 
+https://www.reddit.com/r/AskAnAmerican/comments/18vwq4v/do_americans_think_about_exchange_rates/    
+    - exchange rates change over time
+    - value of currency differs depending on type
+    - USD determines cost of many daily goods 
+    - Americans think about exchange rates when traveling
+        - others find it useful due to value of USD
+    - immigrants acclimating to American life often translate USD back to their home currency to get a better feel for its value    
+
+https://www.riamoneytransfer.com/en/blog/how-exchange-rates-can-impact-you-when-living-abroad/
+    - rates can change by the minute
+    - foreign exchange market runs 24 hours a day, five days a week
+    - economic conditions, inflation, interest rates, and supply and demand all effect exchange rates
+    - Euro and USD are fixed by governments' central banks
+    - airports overcharge
 
 # HMW
 How might we help our client calculate the exchange rates between currencies?
+
+# Ideate
+- A program that supplies updated exchange rates for user's convenience.
+- A program that predicts whether your currency's value is going up or down.
+- A program that converts other currencies to and from USD.
+- A program that helps users budget in multiple currencies. 
+
+# Prototype
+This program can convert between USD and eight other common currencies. It saves a history of each user's purchases in a separate file. The user chooses whether to start or end in USD and inputs a currency and monetary value. The program creates a file under their username and stores the date, the amount, and the currencies converted between. The user can perform more conversions, view their history, or exit the program.
+
+# Reflect
+WWW: I think error handling went well! I was not able to break the program, and neither was my peer tester. Try/except definitely came in handy.
+
+EBI: I wish I could make it so that the exchange rates are updated correctly over time, like if the program got the value from an online source.
+
+IMT: If I had more time, I would add more currencies. I would also make it so that the user didn't have to input their username so often, but I didn't have time to move the variables around and do that (shoutout week five!). I would also do nicer formating. I'd also make the rates more standardized so that the user doesn't need to go between USD when converting.
--- pa3.py
+++ pa3.py
@@ -1,13 +1,6 @@
-'''
-to do:
-readme
-in line comments
-fix whatever is happening with initial input
+import datetime #so that when it is creating a history, it knows the date
 
-'''
-import datetime
-
-convert_to_other_currencies = {
+convert_to_other_currencies = {#exchange rates from dollars to other currencies
     "Euro": 0.87,
     "Japanese yen": 154.19,
     "Pound sterling": 0.76,
@@ -18,7 +11,7 @@ convert_to_other_currencies = {
     "Indian rupee": 88.74
 }
 
-convert_from_other_currencies = {
+convert_from_other_currencies = {#exchange rates from other currencies to dollars
     "Euro": 1.15,
     "Japanese yen": 0.0065,
     "Pound sterling": 1.31,
@@ -32,18 +25,18 @@ convert_from_other_currencies = {
 def calculations(to_or_from):
     currency_options = ['euro', 'euros', 'yen', 'yens', 'japanese yen', 'japanese yens', 'pounds', 'british pounds', 'pound', 'british pound', 'pound sterling', 'cad', 'canadian dollar', 'canadian dollars', 'peso', 'pesos', 'mexican pesos', 'mexican peso', 'chinese yuan', 'chinese yen', 'chinese yuans', 'chinese yens', 'yuans', 'rands', 'sad', 'rand', 'south african rand', 'south african rands', 'rupees', 'rupee', 'ruppee', 'ruppees', 'indian rupee', 'indian rupees']
     
-    if to_or_from == "convert to":
-        currency_name = input("What is currency you would like to convert to? ")     
-        while currency_name.lower() not in currency_options:
+    if to_or_from == "convert to":#using the convert_to dict and language
+        currency_name = input("What currency would you like to convert to? Your options are euros, Japanese yen, British pounds, Canadian dollars, Mexican pesos, Chinese yuan, South African rand, and Indian rupees.\n")     
+        while currency_name.lower() not in currency_options:#if not a valid option
             currency_name = input("Sorry, I didn't recognize that currency. Please try again. ")
-        initial_value = input("What is the original cost? (In dollars) ")
-        while True:
+        initial_value = input("What is the original cost? (In USD) ")
+        while True:#make it into an int, unless if that's not possible (then loop until it is)
             try:
                 int(initial_value)
                 break
             except ValueError:
-                initial_value = input("I didn't recognize that as a number. Please try again. What is the original cost? (In dollars) ")       
-        if currency_name.lower() in ['euro', 'euros']:
+                initial_value = input("I didn't recognize that as a number. Please try again. What is the original cost? (In USD) ")       
+        if currency_name.lower() in ['euro', 'euros']:#converting to exact key so that i can do key/value stuff later
             currency_name = "Euro"
         elif currency_name.lower() in ['yen', 'yens', 'japanese yen', 'japanese yens']:
             currency_name = "Japanese yen"
@@ -59,9 +52,9 @@ def calculations(to_or_from):
             currency_name = "South African rand"
         elif currency_name.lower() in ['rupees', 'rupee', 'ruppee', 'ruppees', 'indian rupee', 'indian rupees']:
             currency_name = "Indian rupee"      
-        currency_rate = convert_to_other_currencies[currency_name] 
+        currency_rate = convert_to_other_currencies[currency_name] #doing key/value stuff to figure out the exchange rate
     elif to_or_from == "convert from":    
-        currency_name = input("What is currency you would like to convert from? ")
+        currency_name = input("What currency would you like to convert from? Your options are euros, Japanese yen, British pounds, Canadian dollars, Mexican pesos, Chinese yuan, South African rand, and Indian rupees.\n")
         while currency_name.lower() not in currency_options:
             currency_name = input("Sorry, I didn't recognize that currency. Please try again. ")
         initial_value = input("What is the original cost? (In another currency) ")
@@ -70,7 +63,7 @@ def calculations(to_or_from):
                 int(initial_value)
                 break
             except ValueError:
-                initial_value = input("I didn't recognize that as a number. Please try again. What is the original cost? (In dollars) ")   
+                initial_value = input("I didn't recognize that as a number. Please try again. What is the original cost? (In USD) ")   
         if currency_name.lower() in ['euro', 'euros']:
             currency_name = "Euro"
         elif currency_name.lower() in ['yen', 'yens', 'japanese yen', 'japanese yens']:
@@ -90,33 +83,40 @@ def calculations(to_or_from):
         while currency_name.lower() not in currency_options:
             currency_name = input("Sorry, I didn't recognize that currency. Please try again. ")      
         currency_rate = convert_from_other_currencies[currency_name]    
-    final_currency = int(initial_value) * int(currency_rate)    
+    final_currency = int(initial_value) * int(currency_rate)#finding out the end value by multiple user-given initial value by the exchange rate     
     print(f"The final answer is {final_currency} in {currency_name}.")
-    return final_currency, currency_name
+    return final_currency, currency_name#returning so other functions can use it
 
-def saving_history(final_currency, currency_name):
+def saving_history(final_currency, currency_name, to_or_from):
     username = input("What is your username? ")
-    filename = f"{username}.txt"
+    filename = f"{username}.txt"#creating a file under the username
     try:
-        file = open(filename, 'x') 
-    except FileExistsError:
+        file = open(filename, 'x') #creating new file
+    except FileExistsError: #if there's already a file under this name, just add to it
         file = open(filename, 'a')
     x = datetime.datetime.now()  
-    todays_date = (x.strftime("%m/%d/%Y"))  
-    file.write(f"{todays_date} - {final_currency}, {currency_name}\n")       
+    todays_date = (x.strftime("%m/%d/%Y")) #this is the format i want for the date
+    if to_or_from == "convert to":
+        file.write(f"{todays_date} - {final_currency}, from USD to {currency_name}\n")  #writing the history down
+    elif to_or_from == "convert from":
+        file.write(f"{todays_date} - {final_currency}, from {currency_name} to USD\n")           
     file.close()     
-    return filename
+    return filename#so other parts of program can use it
 
-def show_history(filename):
-    while True:
+def show_history(filename):#like here!
+    while True:#looping unless the try runs sucessfully, then the loop breaks
         try:
             user_file = open(filename, 'r')
             break
-        except FileNotFoundError:
-            username = input("Sorry, I couldn't find anything under that username. Please try another. ") 
+        except FileNotFoundError:#gives the user a chance to type in a difference file name if this one doesn't exist
+            username = input("Sorry, I couldn't find anything under that username. Please try another or enter exit. ") 
+            if username in ["exit", "quit", "to exit", "exiting", "e"]:#ORRRR they can just exit if they can't remember the user
+                print("Okay, returning to home. \n")
+                return#breaks out of helper function
             filename = f"{username}.txt"
+    print("Your history is: ")        
     file_content = user_file.read()
-    print(file_content)
+    print(file_content)#print history doc (under the username)
 
 
 def main():
@@ -126,23 +126,25 @@ def main():
     exit_options = ["exit", "quit", "to exit", "exiting", "e"]
 
     print("Hi, welcome to Katie's currency!")
-    options = input("Would you like to view past purchases, convert dollars to another currency, convert to dollars from another currency, or exit the program? ")
-    while options.lower() not in exit_options:
-        while options.lower() not in view_options and options.lower() not in convert_to_options and options.lower() not in convert_from_options:
-            options = input("Sorry, I didn't catch that. Would you like to view past purchases, convert dollars to another currency, or convert from dollars to another currency? ")
+    options = input("Would you like to view past purchases, convert USD to another currency, convert to USD from another currency, or exit the program? ")
+    while options.lower() not in exit_options:#while not exit
+        while options.lower() not in view_options and options.lower() not in convert_to_options and options.lower() not in convert_from_options:#if it's not in the options, no can do
+            options = input("Sorry, I didn't catch that. Would you like to view past purchases, convert USD to another currency, or convert from USD to another currency? ")
         if options.lower() in view_options:
             username = input("What is your username? ")
-            fn = f"{username}.txt"
-            show_history(fn) #repeats
+            fn = f"{username}.txt"#basically just filename
+            show_history(fn)
+            options = input("Would you like to view past purchases, convert USD to another currency, convert to USD from another currency, or exit the program? ")#rerun! this couldn't go outside of the if statements because it would just keep looping, because option would just stay the same
         elif options.lower() in convert_to_options:
             to_or_from = "convert to"
             fc, cn = calculations(to_or_from)
-            saving_history(fc, cn) 
+            saving_history(fc, cn, to_or_from) #saves to_or_from so that it knows how to record it in the user's histroy file
+            options = input("\nWould you like to view past purchases, convert USD to another currency, convert to USD from another currency, or exit the program? ")
         elif options.lower() in convert_from_options:      
             to_or_from = "convert from"
             fc, cn = calculations(to_or_from)
-            saving_history(fc, cn) 
-    print("Bye bye!")
-
+            saving_history(fc, cn, to_or_from) 
+            options = input("\nWould you like to view past purchases, convert USD to another currency, convert to USD from another currency, or exit the program? ")
+    print("\nOkay, all done! Your history has been saved. Remember your username to access it again.") #all done!
 if __name__ == "__main__":
     main()    
